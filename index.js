6febe44e839716a0e33c67ae7b8c1fd32c64acee
Jordan <jordanharris1833@yahoo.com>
2024-08-07 19:29:26 -0500

    done

--- index.js
+++ index.js
@@ -11,28 +11,150 @@ div.style.margin = "auto";
 const title = document.createElement("h1");
 title.textContent = "Freelance Forum";
 title.style.textAlign = "center";
-div.appendChild(title);
+div.append(title);
 
 //adding average price text
 const averageText = document.createElement("p");
 averageText.textContent = `The average starting price is `;
 averageText.style.textAlign = "center";
 averageText.style.fontSize = "large";
-div.appendChild(averageText);
+div.append(averageText);
 
 //adding available freelancer title
 const freelanceTitle = document.createElement("h2");
 freelanceTitle.textContent = "Available Freelancers";
 freelanceTitle.style.textAlign = "center";
-div.appendChild(freelanceTitle);
+div.append(freelanceTitle);
+
+//creating a table
+const table = document.createElement("table");
+table.style.margin = "auto";
+table.style.border = "1px solid black";
+div.append(table);
+
+//creating table head
+const thead = document.createElement("thead");
+table.appendChild(thead);
+
+//create table headers
+const tName = document.createElement("th");
+tName.textContent = "Name";
+const tOcc = document.createElement("th");
+tOcc.textContent = "Occupation";
+const tPrice = document.createElement("th");
+tPrice.textContent = "Price";
+thead.appendChild(tName);
+thead.appendChild(tOcc);
+thead.appendChild(tPrice);
+
+//styling table headers
+tName.style.padding = "15px";
+tOcc.style.padding = "15px";
+tPrice.style.padding = "15px";
+
+// creating table body
+const tbody = document.createElement("tbody");
+tbody.style.border = "1px solid black";
+table.style.margin = "auto";
+table.appendChild(tbody);
+
 //freelancer array
-const freelancers = [
-  { name: "Dr. Slice", price: 25, occupation: "gardener" },
-  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-  { name: "Prof. Prism", price: 81, occupation: "teacher" },
-  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-  { name: "Prof. Spark", price: 76, occupation: "programmer" },
-  { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" },
+const names = [
+  "Dr.Slice",
+  "Dr.Pressure",
+  "Prof.Possibility",
+  "Prof.Prism",
+  "Dr.Impulse",
+  "Prof.Spark",
+  "Dr.Wire",
+  "Prof.Goose",
 ];
+const occupation = [
+  "gardener",
+  "programmer",
+  "teacher",
+  "teacher",
+  "programmer",
+  "teacher",
+  "gamer",
+  "diver",
+  "driver",
+];
+const price = [50, 150, 5, 40, 70, 35, 12, 14, 24, 1200];
+const renderNames = [
+  "Dr.Slice",
+  "Dr.Pressure",
+  "Prof.Possibility",
+  "Prof.Prism",
+  "Dr.Impulse",
+  "Prof.Spark",
+  "Dr.Wire",
+  "Prof.Goose",
+];
+const renderOcc = [
+  "gardener",
+  "programmer",
+  "teacher",
+  "teacher",
+  "programmer",
+  "teacher",
+  "gamer",
+  "diver",
+  "driver",
+];
+const renderPrice = [50, 150, 5, 40, 70, 35, 12, 14, 24, 1200];
+const maxPeople = 10;
+
+//set interval
+const randomIntervalId = setInterval(random, 1000);
+
+//render
+nameData();
+
+// //creating functions
+function nameData() {
+  const nameElements = renderNames.map((name, i) => {
+    const row = document.createElement("tr");
+    const nameData = document.createElement("td");
+    const occData = document.createElement("td");
+    const priceData = document.createElement("td");
+    nameData.innerText = name;
+    occData.innerText = renderOcc[i];
+    priceData.innerText = renderPrice[i];
+    row.appendChild(nameData);
+    row.appendChild(occData);
+    row.appendChild(priceData);
+    return row;
+  });
+  tbody.replaceChildren(...nameElements);
+  average();
+}
+
+function random() {
+  const randomName = names[Math.floor(Math.random() * names.length)];
+  const randomOcc = occupation[Math.floor(Math.random() * occupation.length)];
+  const randomPrice = price[Math.floor(Math.random() * price.length)];
+
+  renderNames.push([randomName]);
+  renderOcc.push([randomOcc]);
+  renderPrice.push([randomPrice]);
+
+  if (renderNames.length >= maxPeople) {
+    renderNames.splice(0);
+    renderOcc.splice(0);
+    renderPrice.splice(0);
+  }
+
+  nameData();
+}
+
+function average() {
+  const averagePrices = document.querySelector("p");
+  const averageTotal = renderPrice.length
+    ? renderPrice.reduce((acc, num) => parseInt(acc) + parseInt(num), 0) /
+      renderPrice.length
+    : 0;
+  averagePrices.innerText = `The average starting price is ${parseInt(
+    averageTotal
+  )}`;
+}
